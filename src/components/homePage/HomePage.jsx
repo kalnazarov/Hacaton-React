4000b315cfd39a0d4e1b5e2249f346bc2c75cc54
kalnazarov <sultan4ik2003@mail.ru>
2022-11-29 23:49:29 +0600

    sdfs

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,19 +1,25 @@
 import { Box } from "@mui/system";
 import React from "react";
 import Carousel from "../../pages/Carousel/Carousel";
+import HomeDetals from "../../pages/HomeCard/HomeCard";
 
 const HomePage = () => {
     return (
         <Box
             sx={{
-            
                 width: "100%",
                 height: "100%",
                 backgroundImage: `url(${"https://mobimg.b-cdn.net/v3/fetch/ec/ecc8608ebb9732b0a74cc79493f1a31f.jpeg"})`,
-                backgroundRepeat:'no-repeat',
             }}
         >
             <Carousel />
+            <HomeDetals />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            placeat a molestias, voluptate reprehenderit ex laudantium totam
+            aspernatur assumenda consequuntur, ullam eligendi eveniet ab
+            cupiditate dolores natus officiis ipsa repellat qui dolorem eum rem,
+            ea error dolor. Ab quos libero unde, molestias aspernatur quidem,
+            accusantium quae aut minus, delectus iste.
         </Box>
     );
 };
---
+++ src/pages/HomeCard/HomeCard.jsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, Button, CardActionArea } from "@mui/material";
+import { borderRadius } from "@mui/system";
+
+let image = [
+    {
+        img: "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg",
+        name: "asdasdasdsad",
+        decription: "bbbbbb",
+        price: 313,
+    },
+    {
+        img: "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg",
+        name: "asdasdasdsad",
+        decription: "bbbbbb",
+        price: 313,
+    },
+    {
+        img: "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg",
+        name: "asdasdasdsad",
+        decription: "bbbbbb",
+        price: 313,
+    },
+    {
+        img: "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg",
+        name: "asdasdasdsadas",
+        decription: "bbbbbb",
+        price: 313,
+    },
+];
+
+export default function HomeCard() {
+    return (
+        <Card
+            sx={{
+                display: "flex",
+                justifyContent: "space-evenly",
+
+                flexWrap: "wrap",
+                pt: "50px",
+                
+                backgroundImage: `url(${"https://mobimg.b-cdn.net/v3/fetch/ec/ecc8608ebb9732b0a74cc79493f1a31f.jpeg"})`,
+            }}
+        >
+            {image.map((step) => (
+                <div>
+                    <Box
+                        component="img"
+                        sx={{
+                            width: "100%",
+                            overflow: "hidden",
+
+                            height: "250px",
+                            borderRadius: "10px",
+                        }}
+                        src={step.img}
+                    />
+
+                    <CardActionArea
+                        sx={{
+                            height: "100px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                           
+                            color:'white'
+                        }}
+                    >
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h4"
+                                component="div"
+                            >
+                                {step.name}
+                            </Typography>
+
+                            <Typography
+                               
+                                
+                                sx={{ fontSize: "15px" }}
+                            >
+                                {step.decription}
+                            </Typography>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "space-evenly",
+                                }}
+                            >
+                                <Button>Buy</Button>
+                                <Typography
+                                    gutterBottom
+                                    variant="h6"
+                                    component="div"
+                                >
+                                    ${step.price}
+                                </Typography>
+                            </Box>
+                        </CardContent>
+                    </CardActionArea>
+                </div>
+            ))}
+        </Card>
+    );
+}
